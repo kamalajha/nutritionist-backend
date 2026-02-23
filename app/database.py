b84c819b9911023d902155efe8586e630fc9733f
kamalajha <124757559+kamalajha@users.noreply.github.com>
2026-02-23 16:18:52 +0530

    update to db

--- app/database.py
+++ app/database.py
@@ -1,11 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
+import os
+from dotenv import load_dotenv
 # Use proper PostgreSQL URL
-DATABASE_URL = "postgresql+psycopg2://postgres:mypostgre067@localhost:5432/VirtualMeet"
+load_dotenv()
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
